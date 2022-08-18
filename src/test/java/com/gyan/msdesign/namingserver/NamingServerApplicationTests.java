83e75b1ed091d11adf88074a065d93594006a7ec
gyandeep <gyandeepsharma007@gmail.com>
2022-08-18 14:50:34 +0530

    Initial commit

---
+++ src/test/java/com/gyan/msdesign/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyan.msdesign.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
